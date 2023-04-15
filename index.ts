a4f88e173ce2a6a51d4f561abff82cc609973c87
HAmid <hamidprince823@gmail.com>
2023-04-16 01:28:25 +0500

    completed

---
+++ index.ts
@@ -0,0 +1,34 @@
+import inquirer from 'inquirer'
+let con = true
+async function Todo() {
+    const array = []
+    let condition = true
+    while (condition) {
+
+        const choose = await inquirer.prompt([
+            {
+                name: "choices",
+                type: 'list',
+                message: '',
+                choices: ['add todo', 'exit']
+            },
+
+        ]);
+
+        if (choose.choices == "add todo") {
+            const addTodo = await inquirer.prompt([
+                {
+                    name: "todo",
+                    type: 'input',
+                    message: 'Write the Text',
+                },
+
+            ]);
+            array.push(addTodo.todo);
+            console.log(array);
+
+        } else { condition = false }
+    }
+
+}
+Todo()
